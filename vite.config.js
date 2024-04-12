2f154ceb0f124a11ce058db1bac2f34cb6457810
Ngô Thanh Tâm <22dh114722@st.huflit.edu.vn>
2024-04-13 01:00:52 +0700

    13/04/2024 01:00

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,12 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-// })
+export default defineConfig({
+  plugins: [react(
+    { base: '/grabFood/' }
+  )],
+})
 
-export default {
-  base: '/grabFood/'
-}
+// export default {
+//   base: '/grabFood/'
+// }
